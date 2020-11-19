d5d3cd7f80a3a37c3d2dbe342e2182bf1e673665
NBA81 <natescheinberg@gmail.com>
2020-11-19 15:04:49 -0500

    Update main.js

--- Project6/JS/main.js
+++ Project6/JS/main.js
@@ -4,3 +4,27 @@ function Ride_Function() {
     Can_ride = (Height < 50) ? "You are too small":"You are big enough";
     document.getElementById("Ride").innerHTML = Can_ride + " to ride.";
 }
+
+function Vehicle(Make, Model, Year, Color) {
+	this.Vehicle_Make = Make;
+	this.Vehicle_Model = Model;
+	this.Vehicle_Year = Year;
+	this.Vehicle_Color = Color;
+}
+var Jack = new Vehicle("Dodge", "Viper", 2020, "Red");
+var Emily = new Vehicle("Jeep", "Trail Hawk", 2019, "White and Black");
+var Erik = new Vehicle("Ford", "Pinto", 1971, "Mustard");
+function myFunction() {
+	document.getElementById("Keywords_and_Constructors").innerHTML = 
+	"Erik drives a " + Erik.Vehicle_Color + "-colored " + Erik.Vehicle_Model + " manufactured in " + Erik.Vehicle_Year;
+}
+
+function nestedfunction() {
+	document.getElementById("Nested_Function").innerHTML = Count();
+	function Count() {
+		var Starting_point = 7;
+		function Plus_one() {Starting_point += 1;}
+		Plus_one();
+		return Starting_point;
+	}
+}	
