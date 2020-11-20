2bb78e76520b46ffa970550bfbc2f2f9b78a9316
NBA81 <natescheinberg@gmail.com>
2020-11-20 15:56:04 -0500

    Update tictactoe.js

--- TicTacToe/js/tictactoe.js
+++ TicTacToe/js/tictactoe.js
@@ -2,10 +2,10 @@ let activePlayer = 'X';
 let selectedSquares = [];
 
 function placeXOrO(squareNumber) {
-	if )!selectedSquares.some(element => element.includes(squareNumber))) {
+	if (!selectedSquares.some(element => element.includes(squareNumber))) {
 		let select = document.getElementById(squareNumber);
 		if (activePlayer === 'X') {
-		select.style.backgroundImage = 'url("images/x.png)';
+			select.style.backgroundImage = 'url("images/x.png")';
 		} else {
 			select.style.backgroundImage = 'url("images/o.png")';
 		}
@@ -16,7 +16,8 @@ function placeXOrO(squareNumber) {
 		} else {
 			activePlayer = 'X';
 		}
-		audio('./media/place.mp3');
+		
+		audio('/media/place.mp3');
 		if(activePlayer === 'O'){
 			disableClick();
 			setTimeout(function (){ computersTurn(); }, 1000);
@@ -28,14 +29,14 @@ function placeXOrO(squareNumber) {
 	function computersTurn() {
 		let success = false;
 		let pickASquare;
-		while(!success);
+		while(!success){
 			pickASquare = String(Math.floor(Math.random() * 9));
 			if (placeXOrO(pickASquare)){
 				placeXOrO(pickASquare);
 				success = true;
 			};
+		}
 	}
-}
 }
 
 function checkWinConditions() {
@@ -61,7 +62,7 @@ function checkWinConditions() {
 	else if (arrayIncludes('2O', '5O', '8O')) { drawWinLine(508, 50, 508, 558); }
 	else if (arrayIncludes('6O', '4O', '2O')) { drawWinLine(100, 508, 510, 90); }
 	else if (arrayIncludes('0O', '4O', '8O')) { drawWinLine(100, 100, 520, 520); }
-	else if *selectedSquares.length >= 9) {
+	else if (selectedSquares.length >= 9) {
 		audio('./media/tie.mp3');
 		setTimeout(function () { resetGame(); }, 1000);
 	}
@@ -70,27 +71,68 @@ function checkWinConditions() {
 		const a = selectedSquares.includes(squareA);
 		const b = selectedSquares.includes(squareB);
 		const c = selectedSquares.includes(squareC);
-		if (a === true && b === true && C === true) { return true; }
+		if (a === true && b === true && c === true) { return true; }
 	}
 }
 
-function disableClick
+function resetGame() {
+	for (let i = 0; i < 9; i++) {
+		let square = document.getElementById(String(i));
+		square.style.backgroundImage = '';
+	}
+	selectedSquares = [];
+}
 
+function disableClick() {
+	body.style.pointerEvents = 'none';
+	setTimeout(function() {body.style.pointerEvents = 'auto';}, 1000);
+}
 
+function audio(audioURL) {
+	let audio = new Audio(audioURL);
+	audio.play();
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-			
+function drawWinLine(coordX1, coordY1, coordX2, coordY2) {
+	const canvas = document.getElementById('win-lines');
+	const c = canvas.getContext('2d');
+	let x1 = coordX1,
+		y1 = coordY1,
+		x2 = coordX2,
+		y2 = coordY2,
+		x = x1,
+		y = y1;
 	
+function animateLineDrawing() {
+	const animationLoop = requestAnimationFrame(animateLineDrawing);
+	c.clearRect(0, 0, 608, 608);
+	c.beginPath();
+	c.moveTo(x1, y1);
+	c.lineTo(x, y);
+	c.lineWidth = 10;
+	c.strokeStyle = 'rgba(70, 255, 33, .8)';
+	c.stroke();
+	if (x1 <= x2 && y1 <= y2) {
+		if (x < x2) { x += 10; }
+		if (y < y2) { y += 10; }
+		if (x >= x2 && y >= y2) { cancelAnimationFrame(animationLoop); }
+	}
+	
+	if (x1 <= x2 && y1 >= y2) {
+		if (x < x2) { x += 10; }
+		if (y > y2) { y -= 10; }
+		if (x >= x2 && y <= y2) { cancelAnimationFrame(animationLoop); }
+	}
+}
+
+function clear() {
+	const animationLoop = requestAnimationFrame(clear);
+	c.clearRect(0, 0, 608, 608);
+	cancelAnimationFrame(animationLoop);
+}
+
+disableClick();
+audio('/media/winGame.mp3');
+animateLineDrawing();
+setTimeout(function () { clear(); resetGame(); }, 1000);
+}
